0c22ff00f750f08eeda047384acdcf476fb4dce8
amirdev1883 <amirdev1883@gmail.com>
2025-09-11 04:30:00 +0330

    create Task model and added it to admin panel

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['completed', 'due_date', 'title']
+
+
+    def __str__(self):
+        return f"{self.title} ({'completed' if self.completed else 'Not completed'})"
+

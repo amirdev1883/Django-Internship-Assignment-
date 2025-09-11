98a1f08cc23b38baad3f79297620e11ca3e85c59
amirdev1883 <amirdev1883@gmail.com>
2025-09-11 18:43:14 +0330

    add : User Registeration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.error(request, 'you have already logged in', 'warning')
+            return redirect('tasks:home') 
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect("tasks:home")
